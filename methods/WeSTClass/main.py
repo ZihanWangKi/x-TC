edd310b538653f7d78c4c2c97680b95b4e0d2494
wtl666wtl <2351090351@qq.com>
2022-08-16 13:59:05 +0800

    try

--- methods/WeSTClass/main.py
+++ methods/WeSTClass/main.py
@@ -3,6 +3,25 @@ import random
 import tensorflow as tf
 import os
 
+SEED = 0
+def set_seeds(seed=SEED):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    tf.random.set_seed(seed)
+    tf.set_random_seed(seed)
+    np.random.seed(seed)
+def set_global_determinism(seed=SEED):
+    set_seeds(seed=seed)
+
+    os.environ['TF_DETERMINISTIC_OPS'] = '1'
+    os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
+
+    tf.config.threading.set_inter_op_parallelism_threads(1)
+    tf.config.threading.set_intra_op_parallelism_threads(1)
+
+# Call the above function with seed value
+set_global_determinism(seed=SEED)
+"""
 random_state = 42
 tf.reset_default_graph()
 os.environ['PYTHONHASHSEED'] = str(random_state)
@@ -11,7 +30,7 @@ os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
 np.random.seed(random_state)
 random.seed(random_state)
 tf.set_random_seed(random_state)
-
+"""
 from time import time
 
 # os.environ["CUDA_VISIBLE_DEVICES"]="0"
