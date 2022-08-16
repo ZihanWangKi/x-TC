3b63c978f55dc69b81978540980c4d2c940137f8
wtl666wtl <2351090351@qq.com>
2022-08-16 14:03:07 +0800

    try

--- methods/WeSTClass/main.py
+++ methods/WeSTClass/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 import tensorflow as tf
 import os
-
+import keras.backend as K
 SEED = 0
 def set_seeds(seed=SEED):
     os.environ['PYTHONHASHSEED'] = str(seed)
@@ -15,9 +15,7 @@ def set_global_determinism(seed=SEED):
 
     os.environ['TF_DETERMINISTIC_OPS'] = '1'
     os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
-
-    tf.config.threading.set_inter_op_parallelism_threads(1)
-    tf.config.threading.set_intra_op_parallelism_threads(1)
+    K.set_session(K.tf.Session(config=K.tf.ConfigProto(intra_op_parallelism_threads=30, inter_op_parallelism_threads=30)))
 
 # Call the above function with seed value
 set_global_determinism(seed=SEED)
