5cf7f848e0ca22dd2500e20fd3b95b276b580a81
wtl666wtl <2351090351@qq.com>
2022-08-17 15:08:34 +0800

    Update utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,7 +11,7 @@ def split_data(args):
         dataset = load_dataset(args.dataset, split="train")
         if args.train_size < 1.0:
             train_test = dataset.train_test_split(train_size=args.train_size,
-                                                shuffle=True, seed=args.random_state)
+                                                shuffle=False, seed=args.random_state)
             train_set = train_test["train"]
         else:
             train_set = load_dataset(args.dataset, split="train")
