fdae429fbbd0225167a8581ac15b01a06ba1e2a6
wtl666wtl <2351090351@qq.com>
2022-10-03 20:23:35 +0800

    Update dataset_graph_SSL.py

--- methods/ClassKG/Models/Graph_SSL/dataset_graph_SSL.py
+++ methods/ClassKG/Models/Graph_SSL/dataset_graph_SSL.py
@@ -135,7 +135,8 @@ class Graph_Keywords_Dataset_SSL(DGLDataset):
         print('edge_number:{}'.format(len(src)))
 
         Large_G = dgl.graph((src, dst))
-        print(self.keywords.feature.shape())
+        print(type(self.keywords.feature))
+        print(self.keywords.feature)
         Large_G.ndata['nf'] = self.keywords.feature
         Large_G.edata['ef'] = self.edges.feature['count']
         # draw(Large_G)
