7c12f9ad33be82225df59fe8f87eb5aba71d35b9
wtl666wtl <2351090351@qq.com>
2022-10-24 17:44:20 +0800

    Update run.sh

--- methods/X-Class/scripts/run.sh
+++ methods/X-Class/scripts/run.sh
@@ -6,7 +6,7 @@ seed=$3
 lm=$4
 CUDA_VISIBLE_DEVICES=${gpu} python static_representations.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}
 CUDA_VISIBLE_DEVICES=${gpu} python class_oriented_document_representations.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}
-python document_class_alignment.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}
+python document_class_alignment.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}-12
 python evaluate.py --dataset ${dataset} --stage Rep --suffix ${lm}-12-mixture-100
 python evaluate.py --dataset ${dataset} --stage Align --suffix pca64.clusgmm.${lm}-12.mixture-100.42
 python prepare_text_classifer_training.py --dataset_name ${dataset} --suffix pca64.clusgmm.${lm}-12.mixture-100.42
