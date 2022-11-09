9a6a612150896e8f672aaeb24f7e661ecd0ffb85
wtl666wtl <2351090351@qq.com>
2022-11-09 16:06:48 +0800

    1

--- methods/X-Class/scripts/run.sh
+++ methods/X-Class/scripts/run.sh
@@ -13,7 +13,7 @@ weight_type=$9
 echo $lm
 CUDA_VISIBLE_DEVICES=${gpu} python static_representations.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm} --layer ${layer}
 CUDA_VISIBLE_DEVICES=${gpu} python class_oriented_document_representations.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm} --layer ${layer} --attention_mechanism ${weight_type}
-python document_class_alignment.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}-${layer} --pca ${pca} --document_repr_type ${weight_type}
+python document_class_alignment.py --dataset_name ${dataset} --random_state ${seed} --lm_type ${lm}-${layer} --pca ${pca} --document_repr_type ${weight_type}-100
 python evaluate.py --dataset ${dataset} --stage Rep --suffix ${lm}-${layer}-${weight_type}-100
 python evaluate.py --dataset ${dataset} --stage Align --suffix pca${pca}.clusgmm.${lm}-${layer}.${weight_type}-100.${seed}
 python prepare_text_classifer_training.py --dataset_name ${dataset} --suffix pca${pca}.clusgmm.${lm}-${layer}.${weight_type}-100.${seed}
