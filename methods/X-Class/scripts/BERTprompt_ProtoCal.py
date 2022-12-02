b345cc46e8ed41d955928624927ff01d56561d9f
wtl666wtl <2351090351@qq.com>
2022-12-02 11:24:31 +0800

    Update BERTprompt_ProtoCal.py

--- methods/X-Class/scripts/BERTprompt_ProtoCal.py
+++ methods/X-Class/scripts/BERTprompt_ProtoCal.py
@@ -120,7 +120,7 @@ def main(args):
                     output = model(tokens_tensor.cuda())
                 predictions = output[0]
                 val = predictions[masked_index].item()
-                vec.append(-val)
+                vec.append(val)
             vec = np.array(vec)
             vec = np.exp(vec)
             vec = np.log(vec / vec.sum())
@@ -134,7 +134,10 @@ def main(args):
         gmm.fit(vecs)
         documents_to_class = gmm.predict(vecs)
         centers = gmm.means_
-        row_ind, col_ind = linear_sum_assignment(centers.max() - centers)
+        if args.lm_type != "electra-base" and args.lm_type != "electra-small" and args.lm_type != "electra-large":
+            row_ind, col_ind = linear_sum_assignment(centers.max() - centers)
+        else:
+            row_ind, col_ind = linear_sum_assignment(centers)
         cla = centers[row_ind, col_ind].sum()
         if cla > max_cla:
             max_cla = cla
@@ -144,7 +147,10 @@ def main(args):
     gmm.fit(vecs)
     documents_to_class = gmm.predict(vecs)
     centers = gmm.means_
-    row_ind, col_ind = linear_sum_assignment(centers.max() - centers)
+    if args.lm_type != "electra-base" and args.lm_type != "electra-small" and args.lm_type != "electra-large":
+        row_ind, col_ind = linear_sum_assignment(centers.max() - centers)
+    else:
+        row_ind, col_ind = linear_sum_assignment(centers)
     print("best seed : " + str(best_seed))
     print("class center :")
     print(centers)
@@ -194,7 +200,7 @@ def main(args):
                     output = model(tokens_tensor.cuda())
                 predictions = output[0]
                 val = predictions[masked_index].item()
-                vec.append(-val)
+                vec.append(val)
             vec = np.array(vec)
             vec = np.exp(vec)
             vec = np.log(vec / vec.sum())
