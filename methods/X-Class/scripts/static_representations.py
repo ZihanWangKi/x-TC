1cc39ee42fee22a86a98f88f0becc1f8b4eac525
wtl666wtl <2351090351@qq.com>
2022-12-17 16:12:53 +0800

    Update static_representations.py

--- methods/X-Class/scripts/static_representations.py
+++ methods/X-Class/scripts/static_representations.py
@@ -193,7 +193,7 @@ def main(args):
         else:
             Len =  768
         _pca = PCA(n_components=Len, random_state=args.random_state)
-        static_word_representations -= _pca.fit_transform(static_word_representations)[:, 0] * np.array([1 for _ in range(len(static_word_representations))])
+        static_word_representations -= _pca.fit_transform(static_word_representations)[:, 0:1] * np.array([1 for _ in range(len(static_word_representations))])
         print(f"Explained variance: {sum(_pca.explained_variance_ratio_)}")
 
     with open(os.path.join(data_folder, f"tokenization_lm-{args.lm_type}-{args.layer}.pk"), "wb") as f:
