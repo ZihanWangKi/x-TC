0e35381658f245f1db7ac9e3c30006b7dd343565
wtl666wtl <2351090351@qq.com>
2022-12-19 21:52:41 +0800

    Update logging.py

--- methods/NPPrompt/openprompt/utils/logging.py
+++ methods/NPPrompt/openprompt/utils/logging.py
@@ -1,150 +1,85 @@
-from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score
-from typing import *
-from openprompt.utils.logging import logger
+# -*- coding: utf-8 -*-
+import logging
+import os
+import datetime
+
+logger = logging.getLogger()
 
 
-def f1(p, r):
-    if p == 0. or r == 0.:
-        return 0.
-    return 2 * p * r / (p + r)
-
-
-def label_path(label, label_path_sep):
-    label = label.strip(label_path_sep)
-    label_path = label.split(label_path_sep)
-    label_set = []
-    for i in range(len(label_path)):
-        label_set.append(label_path_sep.join(label_path[:i + 1]))
-    return label_set
-
-
-def loose_micro(labels, preds, id2label, label_path_sep):
-    if id2label is None:
-        raise ValueError("no id2label dict provided, cannot calculate loose_micro_f1 !")
-    labels = [label_path(id2label[i], label_path_sep) for i in labels]
-    preds = [label_path(id2label[i], label_path_sep) for i in preds]
-    cnt_pred = 0
-    cnt_label = 0
-    cnt_correct = 0
-    for label, pred in zip(labels, preds):
-        label = set(label)
-        pred = set(pred)
-        cnt_pred += len(pred)
-        cnt_label += len(label)
-        cnt_correct += len(label.intersection(pred))
-    p = cnt_correct / cnt_pred
-    r = cnt_correct / cnt_label
-    f = f1(p, r)
-    return {'precision': p, 'recall': r, 'f1': f}
-
-
-def loose_macro(labels, preds, id2label, label_path_sep):
-    if id2label is None:
-        raise ValueError("no id2label dict provided, cannot calculate loose_micro_f1 !")
-    labels = [label_path(id2label[i], label_path_sep) for i in labels]
-    preds = [label_path(id2label[i], label_path_sep) for i in preds]
-    p = 0.
-    r = 0.
-    for label, pred in zip(labels, preds):
-        label = set(label)
-        pred = set(pred)
-        if len(pred) > 0:
-            p += len(label.intersection(pred)) / len(pred)
-        if len(label) > 0:
-            r += len(label.intersection(pred)) / len(label)
-    p /= len(labels)
-    r /= len(labels)
-    f = f1(p, r)
-    return {'precision': p, 'recall': r, 'f1': f}
-
-
-def classification_metrics(preds: Sequence[int],
-                           labels: Sequence[int],
-                           metric: Optional[str] = "micro-f1",
-                           id2label: Optional[Dict] = None,
-                           label_path_sep: Optional[str] = '-',
-                           ) -> float:
-    """evaluation metrics for classification task.
-
-    Args:
-        preds (Sequence[int]): predicted label ids for each examples
-        labels (Sequence[int]): gold label ids for each examples
-        metric (str, optional): type of evaluation function, support 'micro-f1', 'macro-f1', 'accuracy', 'precision', 'recall'. Defaults to "micro-f1".
-
-    Returns:
-        score (float): evaluation score
+def config_experiment_dir(config):
+    r""" Automatic generate log directory for experiments.
+    First generate the unique_string of one experiment, if the user
+    didn't specify one, according
+    to the user-defined keys logging.unique_string_keys.
+    Then create the directory.
     """
+    if not os.path.exists(config.logging.path_base):
+        raise NotADirectoryError(f"logging base directory `{config.logging.path_base}` not found, you should create one.")
 
-    if metric == "micro-f1":
-        score = f1_score(labels, preds, average='micro')
-    elif metric == "macro-f1":
-        score = f1_score(labels, preds, average='macro')
-    elif metric == "accuracy":
-        score = accuracy_score(labels, preds)
-    elif metric == "precision":
-        score = precision_score(labels, preds)
-    elif metric == "recall":
-        score = recall_score(labels, preds)
-    # only hierarchical label loose metric is supported TODO naive multilabel ?
-    elif metric == 'loose-micro-f1':
-        score = loose_micro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['f1']
-    elif metric == 'loose-macro-f1':
-        score = loose_macro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['f1']
-    elif metric == 'loose-micro-precision':
-        score = loose_micro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['precision']
-    elif metric == 'loose-macro-precision':
-        score = loose_macro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['precision']
-    elif metric == 'loose-micro-recall':
-        score = loose_micro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['recall']
-    elif metric == 'loose-macro-recall':
-        score = loose_macro(labels, preds, id2label=id2label, label_path_sep=label_path_sep)['recall']
+    # generate unique string
+    temp_strs = []
+    if config.logging.unique_string is None:
+        for item in config.logging.unique_string_keys:
+            if item == "datetime":
+                continue
+            item = item.split(".")  # split into sub items.
+            subconfig = config
+            for key in item:
+                try:
+                    subconfig = subconfig[key]
+                except:
+                    raise ValueError("The unique string_key is not a config option ")
+            if not isinstance(subconfig, str):
+                try:
+                    subconfig = str(subconfig)
+                except:
+                    print("The value of subconfig key {} can't be converted to a string".format(".".join(item)))
+                    continue
+
+            subconfig = subconfig.split("/")[-1]
+            temp_strs.append(subconfig)
+
+        if 'datetime' in config.logging.unique_string_keys:
+            if config.logging.datetime_format is None:
+                config.logging.datetime_format = '%y%m%d%H%M%S'
+            time_str = datetime.datetime.now().strftime(config.logging.datetime_format)
+            temp_strs.append(time_str)
+        config.logging.unique_string = "_".join(temp_strs)
+    config.logging.path = os.path.join(config.logging.path_base, config.logging.unique_string)
+
+    # create the log directory
+    if os.path.exists(config.logging.path):
+        if config.logging.overwrite:
+            import shutil
+            shutil.rmtree(config.logging.path)
+            os.mkdir(config.logging.path)
+        else:
+            raise FileExistsError("Log dir {} exists and can't overwrite!")
     else:
-        raise ValueError("'{}' is not a valid evaluation type".format(metric))
-    return score
+        os.mkdir(config.logging.path)
+    return config.logging.path
 
 
-def generation_metric(hypos,
-                      refs,
-                      metric: Optional[str] = "sentence_bleu"):
-    r"""Some basic metric function for generation. However, many generation tasks
-    has their own evaluation bash scripts.
+def init_logger(
+        log_file,
+        log_file_level=logging.NOTSET,
+        log_level=logging.INFO,
+):
+    if isinstance(log_file_level, str):
+        log_file_level = getattr(logging, log_file_level)
+    if isinstance(log_level, str):
+        log_level = getattr(logging, log_level)
+    log_format = logging.Formatter("[\033[032m%(asctime)s\033[0m %(levelname)s] %(module)s.%(funcName)s %(message)s")
+    logger = logging.getLogger()
+    logger.setLevel(log_level)
 
-    Args:
-        hypos (:obj:`str`) : the generated sentence.
-        refs (:obj:`list(str)`) : the referenced (ground-truth) sentence.
-        metric (:obj:`str`, `optional`) : the type of metric option
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_format)
+    logger.handlers = [console_handler]
 
-    Returns:
-        score (float): evaluate score
-    """
-    if metric == "sentence_bleu":
-        # a simple criterion to visualize the performance, not rigorous.
-        import nltk
-        try:
-            nltk_path = str(nltk.data.find("tokenizers/punkt"))
-            logger.info(f"using nltk from: {nltk_path}")
-        except LookupError:
-            nltk.download('punkt')
-
-        from nltk.translate.bleu_score import sentence_bleu
-        from nltk.tokenize import word_tokenize
-        from nltk.translate.bleu_score import SmoothingFunction
-        smoothie = SmoothingFunction().method4  # a function for smooth
-        scores = []
-
-        for ref, hypo in zip(refs, hypos):
-            tokenized_rs = []
-            ref = ref.split("\n")
-            for r in ref:
-                tokenized_rs.append(word_tokenize(r))
-            hypo = word_tokenize(hypo)
-            try:
-                sc = sentence_bleu(tokenized_rs, hypo, smoothing_function=smoothie)
-            except ValueError:  # TODO ZeroDivisionError
-                logger.warning("math domain error in bleu, set to 0.0. generated sentence: {}".format(hypo))
-                sc = 0.0
-            scores.append(sc)
-        score = sum(scores) / len(scores)
-        return score
-    else:
-        raise ValueError("'{}' is not a valid metric type.".format(metric))
+    if log_file and log_file != '':
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setLevel(log_file_level)
+        file_handler.setFormatter(log_format)
+        logger.addHandler(file_handler)
+    return logger
