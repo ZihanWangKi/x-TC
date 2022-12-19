127617cba3f6846821a8ed18fbd674949b831644
wtl666wtl <2351090351@qq.com>
2022-12-19 21:59:04 +0800

    Update trainer.py

--- methods/NPPrompt/openprompt/trainer.py
+++ methods/NPPrompt/openprompt/trainer.py
@@ -27,7 +27,7 @@ from openprompt.utils.logging import logger
 from openprompt.utils.metrics import classification_metrics, generation_metric
 from transformers import AdamW, get_linear_schedule_with_warmup
 from transformers.optimization import Adafactor, AdafactorSchedule
-
+from yacs.config import CfgNode
 
 class BaseRunner(object):
     r"""A base runner for training without training tricks.
