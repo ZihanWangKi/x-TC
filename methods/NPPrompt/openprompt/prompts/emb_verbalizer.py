6832ec12607e7a2affa0859d9734e1477897b817
wtl666wtl <2351090351@qq.com>
2022-12-20 09:29:13 +0800

    Update emb_verbalizer.py

--- methods/NPPrompt/openprompt/prompts/emb_verbalizer.py
+++ methods/NPPrompt/openprompt/prompts/emb_verbalizer.py
@@ -163,8 +163,9 @@ class EmbVerbalizer(Verbalizer):
         print('Dataset name:', dataset_name)
         dir = f"./datasets/" + dataset_name + "/class_names.txt"
         with open(dir, "r") as fp:
-            class_names = list(map(lambda x: ' ' + x.strip().lower(), fp.readlines()))
-        class_names = [[_] for _ in class_names]
+            class_names_ = list(map(lambda x: ' ' + x.strip().lower(), fp.readlines()))
+        class_names = [[_] for _ in class_names_]
+        print(class_names)
         """
         if dataset_name == 'agnews':
             class_names = [[" world", " politics"], [" sports"], [" business"], [" technology", ' science']]
