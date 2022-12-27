597a2b90aa048b76a362a87b78a84f151fc97a69
wtl666wtl <2351090351@qq.com>
2022-12-27 22:45:37 +0800

    try

--- scripts/utils.py
+++ scripts/utils.py
@@ -369,8 +369,8 @@ def run_method(args, train_set, test_set):
         assert args.seed_words == False
         if args.n_shot == 0:
             os.system("mkdir -p ../methods/GPT/data/{}".format(args.dataset))
-            os.system("cp class_names.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
-            os.system("cp prompt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
+            os.system("cp class_names_gpt.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
+            os.system("cp prompt_gpt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
             with open("../methods/GPT/data/{}/train.txt".format(args.dataset), "w") as f:
                 for line in train_set["x-TC"]:
                     f.write(str(line))
@@ -393,8 +393,8 @@ def run_method(args, train_set, test_set):
                 .format(args.gpu, args.dataset, args.method, args.random_state))
         else:
             os.system("mkdir -p ../methods/GPT/data/{}".format(args.dataset))
-            os.system("cp class_names.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
-            os.system("cp prompt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
+            os.system("cp class_names_gpt.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
+            os.system("cp prompt_gpt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
             A = np.random.permutation(np.arange(len(train_set["x-TC"])))
             n_shot = []
             label_num = [0 for _ in range(max(train_set[args.label_name])+1)]
@@ -430,8 +430,8 @@ def run_method(args, train_set, test_set):
         assert args.class_names == True
         assert args.seed_words == False
         os.system("mkdir -p ../methods/GPT/data/{}".format(args.dataset))
-        os.system("cp class_names.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
-        os.system("cp prompt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
+        os.system("cp class_names_gpt.txt ../methods/GPT/data/{}/class_names.txt".format(args.dataset))
+        os.system("cp prompt_gpt.txt ../methods/GPT/data/{}/prompt.txt".format(args.dataset))
         with open("../methods/GPT/data/{}/train.txt".format(args.dataset), "w") as f:
             for line in train_set["x-TC"]:
                 f.write(str(line))
