5bd58b4e91499b5d21ac548990aa4e1f24b88702
wtl666wtl <2351090351@qq.com>
2022-12-29 15:02:21 +0800

    try

--- scripts/dataprocess.py
+++ scripts/dataprocess.py
@@ -1,23 +1,25 @@
 import json
 import os
 
-with open("../data/NYT-fine/unlabeled.json", 'r') as f:
+with open("../data/20News-fine/unlabeled.json", 'r') as f:
     train_list = json.load(f)
 
 texts = []
 labels = []
 for i in range(len(train_list)):
     text, label = train_list[i]
+    if label == 6 or label == 18 or label == 19:
+        continue
     texts.append(text)
     labels.append(label)
 
 print(labels[:10])
 
-with open("../data/NYT-fine/labels.txt", 'w') as f:
+with open("../data/20News-fine/labels.txt", 'w') as f:
     for i in range(len(labels)):
         f.write(str(labels[i]))
         f.write("\n")
-with open("../data/NYT-fine/dataset.txt", 'w') as f:
+with open("../data/20News-fine/dataset.txt", 'w') as f:
     for i in range(len(texts)):
         f.write(str(texts[i]))
         f.write("\n")
