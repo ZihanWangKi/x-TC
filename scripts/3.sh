196063b45bf19111730593b149932e3d3836c253
wtl666wtl <2351090351@qq.com>
2023-01-13 21:29:58 +0800

    try

--- scripts/3.sh
+++ scripts/3.sh
@@ -1,2 +1,8 @@
-taskset -c 90-110 python run.py  --method gpt2-xl --dataset 20News-fine --class_names_file ./default_class_names/20-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 6 --prompt --suffix "--iter 100" --additional_method ProtoCal
-taskset -c 90-110 python run.py  --method gpt2-xl --dataset NYT-fine --class_names_file ./default_class_names/nyt-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 6 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/1.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/1.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/2.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/2.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/3.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/3.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/4.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-small  --class_names_file ./agnews/4.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
---
+++ scripts/4.sh
@@ -0,0 +1,8 @@
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/1.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/1.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/2.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/2.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/3.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/3.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/4.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100"
+taskset -c 30-60 python run.py  --method gpt2-medium  --class_names_file ./agnews/4.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu 1 --prompt --suffix "--iter 100" --additional_method ProtoCal
