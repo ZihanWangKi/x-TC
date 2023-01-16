0abbbbd3dd937e8d46ede99ae6d42a9fc6d0e796
wtl666wtl <2351090351@qq.com>
2023-01-16 13:24:25 +0800

    Update trainer.py

--- methods/LOTClass/src/trainer.py
+++ methods/LOTClass/src/trainer.py
@@ -155,7 +155,8 @@ class LOTClassTrainer(object):
                 print("Locating label names in the corpus.")
                 chunk_size = ceil(len(docs) / self.num_cpus)
                 chunks = [docs[x:x+chunk_size] for x in range(0, len(docs), chunk_size)]
-                results = Parallel(n_jobs=self.num_cpus)(delayed(self.label_name_occurrence)(docs=chunk) for chunk in chunks)
+                #results = Parallel(n_jobs=self.num_cpus)(delayed(self.label_name_occurrence)(docs=chunk) for chunk in chunks)
+                results = [self.label_name_occurrence(docs=chunk) for chunk in chunks]
                 input_ids_with_label_name = torch.cat([result[0] for result in results])
                 attention_masks_with_label_name = torch.cat([result[1] for result in results])
                 label_name_idx = torch.cat([result[2] for result in results])
