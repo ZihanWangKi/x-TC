9d72c5def5c10d9dd3745f262d802f471f754314
wtl666wtl <2351090351@qq.com>
2023-01-16 14:56:00 +0800

    Update model_roberta.py

--- methods/LOTClass/src/model_roberta.py
+++ methods/LOTClass/src/model_roberta.py
@@ -9,7 +9,7 @@ class LOTClassModel_roberta(RobertaPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
-        self.bert = RobertaModel(config, add_pooling_layer=False)
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
         self.cls = RobertaLMHead(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.dense = nn.Linear(config.hidden_size, config.hidden_size)
@@ -22,7 +22,7 @@ class LOTClassModel_roberta(RobertaPreTrainedModel):
 
     def forward(self, input_ids, pred_mode, attention_mask=None, token_type_ids=None,
                 position_ids=None, head_mask=None, inputs_embeds=None):
-        bert_outputs = self.bert(input_ids,
+        bert_outputs = self.roberta(input_ids,
                                  attention_mask=attention_mask,
                                  token_type_ids=token_type_ids,
                                  position_ids=position_ids,
