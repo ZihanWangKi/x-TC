a248f2678dc2b15d87188b0edcfe72d298de37e9
wtl666wtl <2351090351@qq.com>
2023-01-18 22:32:56 +0800

    fix

--- scripts/orz/a2.sh
+++ scripts/orz/a2.sh
@@ -1,4 +1,4 @@
 gpu=$1
 
-MASTER_PORT=10670 taskset -c 60-90 python run.py  --method ClassKG --dataset NYT-fine --class_names_file ./default_class_names/nyt-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "clustering"
+MASTER_PORT=10670 taskset -c 60-90 python run.py  --method ClassKG --dataset NYT-fine --class_names_file ./default_class_names/nyt-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "clustering "
 MASTER_PORT=10670 taskset -c 60-90 python run.py  --method ClassKG --dataset NYT-fine --class_names_file ./default_class_names/nyt-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "--lm bert-large-uncased"
