7eb737a0d783ab8563d3d4b07087270c2bfe8470
wtl666wtl <2351090351@qq.com>
2023-01-20 21:07:21 +0800

    Update XClass.sh

--- scripts/XClass.sh
+++ scripts/XClass.sh
@@ -37,8 +37,8 @@ gpu=$1
 #taskset -c 30-60 python run.py  --dataset NYT --class_names_file ./nyt/4.txt --prompt_file ./prompts/topic.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture gmm"
 #taskset -c 30-60 python run.py  --dataset NYT --class_names_file ./nyt/4.txt --prompt_file ./prompts/topic.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "100 blu 24 blc 128 mixture gmm"
 
-taskset -c 30-60 python run.py  --dataset NYT-Topics --class_names_file ./default_class_names/nyt-top.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
-taskset -c 30-60 python run.py  --dataset NYT-Locations --class_names_file ./default_class_names/nyt-loc.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
+taskset -c 30-60 python run.py  --dataset NYT-Topics --class_names_file ./default_class_names/nyt-top.txt --split_ratio 0.8 --class_names --train_size 0.2 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
+taskset -c 30-60 python run.py  --dataset NYT-Locations --class_names_file ./default_class_names/nyt-loc.txt --split_ratio 0.8 --class_names --train_size 0.2 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 taskset -c 30-60 python run.py  --dataset NYT-fine --class_names_file ./default_class_names/nyt-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 
 #taskset -c 30-60 python run.py  --class_names_file ./default_class_names/ag.txt --split_ratio 0.7 --class_names --train_size 0.05 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
