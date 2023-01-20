2eb05f64ecd36c071a0c8d8d714a5107cded4f2c
wtl666wtl <2351090351@qq.com>
2023-01-21 00:01:40 +0800

    final？

--- scripts/review/XClass2.sh
+++ scripts/review/XClass2.sh
@@ -2,8 +2,6 @@ gpu=$1
 taskset -c 60-90 python run.py  --dataset 20News-fine --class_names_file ./default_class_names/20-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 taskset -c 60-90 python run.py  --dataset 20News --class_names_file ./default_class_names/20.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 
-taskset -c 60-90 python run.py  --dataset yelp_polarity --class_names_file ./default_class_names/review.txt --split_ratio 0.7 --class_names --train_size 0.01 --test_size 0.1 --gpu ${gpu} --suffix "64 roberta-base 12 roberta-base 128 mixture gmm"
-taskset -c 60-90 python run.py  --dataset yelp_polarity --class_names_file ./default_class_names/review.txt --split_ratio 0.7 --class_names --train_size 0.01 --test_size 0.1 --gpu ${gpu} --suffix "100 roberta-large 24 roberta-large 128 mixture gmm"
+taskset -c 60-90 python run.py  --dataset NYT --class_names_file ./default_class_names/nyt.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
+taskset -c 60-90 python run.py  --dataset dbpedia_14 --text_name title content --class_names_file ./default_class_names/dbpedia.txt --split_ratio 0.7 --class_names --train_size 0.01 --test_size 0.1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 
-taskset -c 60-90 python run.py   --class_names_file ./default_class_names/ag.txt --split_ratio 0.7 --class_names --train_size 0.05 --test_size 1 --gpu ${gpu} --suffix "64 roberta-base 12 roberta-base 128 mixture gmm"
-taskset -c 60-90 python run.py   --class_names_file ./default_class_names/ag.txt --split_ratio 0.7 --class_names --train_size 0.05 --test_size 1 --gpu ${gpu} --suffix "100 roberta-large 24 roberta-large 128 mixture gmm"
