e354b11da6adbed5737c733f9622c9c77a82f2fe
wtl666wtl <2351090351@qq.com>
2023-01-21 00:24:09 +0800

    fix

--- scripts/review/XClass2.sh
+++ scripts/review/XClass2.sh
@@ -2,6 +2,3 @@ gpu=$1
 taskset -c 60-90 python run.py  --dataset 20News-fine --class_names_file ./default_class_names/20-fine.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 taskset -c 60-90 python run.py  --dataset 20News --class_names_file ./default_class_names/20.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
 
-taskset -c 60-90 python run.py  --dataset NYT --class_names_file ./default_class_names/nyt.txt --split_ratio 0.7 --class_names --train_size 0.5 --test_size 1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
-taskset -c 60-90 python run.py  --dataset dbpedia_14 --text_name title content --class_names_file ./default_class_names/dbpedia.txt --split_ratio 0.7 --class_names --train_size 0.01 --test_size 0.1 --gpu ${gpu} --suffix "64 bbu 12 bbc 128 mixture none"
-
